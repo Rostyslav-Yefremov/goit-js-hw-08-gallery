8e94c1ae55cc5ca1a02da5ebb33d50cdcc22960d
Rostyslav-Yefremov <rostislavefremow@gmail.com>
2021-10-05 19:34:22 +0300

    refact

--- app.js
+++ app.js
@@ -64,47 +64,130 @@ const galleryItems = [
     },
   ];
 
-const galleryRef = document.querySelector('.js-gallery');
-const openModalRef = document.querySelector('.js-lightbox');
-const lightboxImagesRef = document.querySelector('.lightbox__image');
-const gallery = document.querySelectorAll('.gallery');
-const closeModalBtnRef = document.querySelector('[data-action="close-lightbox"]');
-const modalContentRef = document.querySelector('.lightbox__content');
+
+const gallery = document.querySelector('.js-gallery');
+const lightbox = document.querySelector('.js-lightbox');
+const overlay = document.querySelector('.lightbox__overlay');
+const imgLightbox = document.querySelector('.lightbox__image');
+const btnLightbox = document.querySelector('button[data-action="close-lightbox"]');
+
+
+const createGallery = ({ original, preview, description }) => {
+  return `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href=${original}
+  >
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</li>
+`;
+};
+
+const markup = galleryItems.map(item => createGallery(item)).join('');
+gallery.insertAdjacentHTML('beforeend', markup);
+
+// Реализация делегирования на галерее
+const openModal = event => {
+  event.preventDefault();
+
+  if (event.target === event.currentTarget) return;
+
+  lightbox.classList.add('is-open');
+  imgLightbox.src = event.target.getAttribute('data-source');
+  imgLightbox.alt = event.target.alt;
+
+  window.addEventListener('keydown', onPressESC);
+  gallery.addEventListener('keydown', onPressArrow);
+};
+
+const closeModal = () => {
+  lightbox.classList.remove('is-open');
+  imgLightbox.src = '';
+  imgLightbox.alt = '';
+
+  window.removeEventListener('keydown', onPressArrow);
+};
+
+const onOverlayClick = event => {
+  if (event.target === event.currentTarget) closeModal();
+};
+
+const onPressESC = event => {
+  if (event.code === 'Escape') closeModal();
+};
+
+const onPressArrow = event => {
+  let activeIndex = galleryItems.findIndex(image => image.original === imgLightbox.src);
+
+  if (event.code === 'ArrowRight') {
+    activeIndex < galleryItems.length - 1 ? (activeIndex += 1) : 0;
+  }
+
+  if (event.code === 'ArrowLeft') {
+    activeIndex > 0 ? (activeIndex -= 1) : galleryItems.length - 1;
+  }
+
+  imgLightbox.src = galleryItems[activeIndex].original;
+  imgLightbox.alt = galleryItems[activeIndex].alt;
+};
+
+// Слушатели
+gallery.addEventListener('click', openModal);
+btnLightbox.addEventListener('click', closeModal);
+overlay.addEventListener('click', onOverlayClick);
+
+
+
+
+
+
+// const galleryRef = document.querySelector('.js-gallery');
+// const openModalRef = document.querySelector('.js-lightbox');
+// const lightboxImagesRef = document.querySelector('.lightbox__image');
+// const gallery = document.querySelectorAll('.gallery');
+// const closeModalBtnRef = document.querySelector('[data-action="close-lightbox"]');
+// const modalContentRef = document.querySelector('.lightbox__content');
 
 // Создание и рендер разметки по массиву
 
-const galleryList = galleryItems.reduce((acc, img) => {
-  let createImage = `<li class="gallery__item"><a class="gallery__link"><img src="${img.preview}" alt="${img.description}" class="gallery__image" ></a></li>`
-  return acc + createImage
-}, ''); 
+// const galleryList = galleryItems.reduce((acc, img) => {
+//   let createImage = `<li class="gallery__item"><a class="gallery__link"><img src="${img.preview}" alt="${img.description}" class="gallery__image" ></a></li>`
+//   return acc + createImage
+// }, ''); 
 
-galleryRef.insertAdjacentHTML('beforeend', galleryList);
+// galleryRef.insertAdjacentHTML('beforeend', galleryList);
 
 
 // Oткрытие модального окна по клику на элементе галереи. Подмена значения атрибута src
 
-galleryRef.addEventListener('click', (e) => {
-    galleryItems.forEach((elem) => {
-      if (e.target.alt === elem.description) {
-        lightboxImagesRef.src = elem.original
-        lightboxImagesRef.alt = elem.description
-      }
-      openModalRef.classList.add('is-open')
-  })})
+// galleryRef.addEventListener('click', (e) => {
+//     galleryItems.forEach((elem) => {
+//       if (e.target.alt === elem.description) {
+//         lightboxImagesRef.src = elem.original
+//         lightboxImagesRef.alt = elem.description
+//       }
+//       openModalRef.classList.add('is-open')
+//   })})
 
   // Закрытие модального окна и очистка значения атрибута src элемента
 
-openModalRef.addEventListener('click', (e) => {
-    if (e.target.nodeName === 'IMG') {
-      return
-    }
-    if (e.target.nodeName === closeModalBtnRef) {
-      openModalRef.classList.remove('is-open')
-      lightboxImagesRef.src = '';
-      lightboxImagesRef.alt = '';
-      return
-    }
-    openModalRef.classList.remove('is-open')
-    lightboxImagesRef.src = '';
-    lightboxImagesRef.alt = '';
-  })
+// openModalRef.addEventListener('click', (e) => {
+//     if (e.target.nodeName === 'IMG') {
+//       return
+//     }
+//     if (e.target.nodeName === closeModalBtnRef) {
+//       openModalRef.classList.remove('is-open')
+//       lightboxImagesRef.src = '';
+//       lightboxImagesRef.alt = '';
+//       return
+//     }
+//     openModalRef.classList.remove('is-open')
+//     lightboxImagesRef.src = '';
+//     lightboxImagesRef.alt = '';
+//   })
